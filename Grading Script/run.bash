c5a3fd302321bdcefff106b2dbe39bea86a83244
Jeff Dong <85254367+Jeffafuh@users.noreply.github.com>
2021-11-11 21:01:30 -0600

    Grading Script upload

---
+++ Grading Script/run.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Use this to easily run the script~
+# Must provide the input file as arg1, optional output file as arg2
+
+if [[ $# -eq 2 ]]; then
+    `awk -f formatGrades.awk $1 > $2`
+    exit 0
+fi
+
+if [[ $# -eq 1 ]]; then
+    `awk -f formatGrades.awk $1 > out.txt`
+    exit 0
+fi
+
+echo "Usage: $0 inFile [outFile]"
+exit 1
